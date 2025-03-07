3fa7745ab057a808e3726f7c35ec6ef27d871af0
mbh226 <molly.herforth7@gmail.com>
2025-03-07 16:14:32 -0500

    Delete error_handler.py

--- error_handler.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_command(command, log_file_path):
-    with open(log_file_path, "w") as log_file:
-        result = subprocess.run(command, shell=True, stdout=log_file, stderr=log_file)
-        if result.returncode != 0:
-            print(f"Error occurred. See log file: {log_file_path}")
-            exit(1)
