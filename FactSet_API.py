eb1f8566280132e77b8942ba1247fbedf4754022
Zoe Barrington <zab22@cornell.edu>
2025-04-08 08:59:34 -0400

    Update FactSet_API.py

--- FactSet_API.py
+++ FactSet_API.py
@@ -2,37 +2,40 @@
 import pandas as pd
 import json
 
-# FDS Packages 
-import fds.sdk.FactSetPrices  # To install: "pip install fds.sdk.utils fds.sdk.FactSetPrices==1.1.7"
-from fds.sdk.FactSetPrices.api import prices_api
-from fds.sdk.FactSetPrices.models import *
+# FDS Packages
 from fds.sdk.utils.authentication import ConfidentialClient
+import fds.sdk.OverviewReportBuilder
+from fds.sdk.OverviewReportBuilder.api import company_api
+from fds.sdk.OverviewReportBuilder.models import *
+from pprint import pprint
 
 # Path to configuration file
 config_file_path = '/content/factor-lake/FactSet_Colab_Test-config.json'
 
 # Authentication
-configuration = fds.sdk.FactSetPrices.Configuration(
-    fds_oauth_client=ConfidentialClient(config_file_path)  # Pass the config file path directly
+configuration = fds.sdk.OverviewReportBuilder.Configuration(
+    fds_oauth_client=ConfidentialClient(config_file_path)
 )
 
-# Get fixed-income data
-with fds.sdk.FactSetPrices.ApiClient(configuration) as api_client:
+# Retrieve company data
+with fds.sdk.OverviewReportBuilder.ApiClient(configuration) as api_client:
     # Create an instance of the API class
-    api_instance = prices_api.PricesApi(api_client)
+    api_instance = company_api.CompanyApi(api_client)
+    id = "FDS"  # Replace with valid company ticker
 
-    prices_fixed_income_request = PricesFixedIncomeRequest(
-        ids=FixedIds(["037833BX"]),  # Replace with valid security IDs
-        start_date="2019-01-01",     # Replace with desired start date
-        end_date="2019-12-31",       # Replace with desired end date
-        frequency=FrequencyFi("M"),  # Frequency: "M" for monthly
-    )
-
-    # Send Request
     try:
-        api_response = api_instance.get_fixed_security_prices_for_list(prices_fixed_income_request)
-        # Convert to Pandas DataFrame
-        results = pd.json_normalize([a.__dict__["_data_store"] for a in api_response["data"]])
-        print(results)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        # Fetch current capitalization
+        api_response = api_instance.get_current_cap(id)
+
+        # Convert API response to Pandas DataFrame
+        response_dict = api_response.to_dict()['data']
+        simple_json_response = pd.DataFrame(response_dict)
+        nested_json_response = pd.json_normalize(response_dict)
+
+        # Print the DataFrames
+        print("Simple JSON Response:")
+        print(simple_json_response)
+        print("\nNested JSON Response:")
+        print(nested_json_response)
+    except fds.sdk.OverviewReportBuilder.ApiException as e:
+        print(f"Exception when calling CompanyApi->get_current_cap: {e}")
