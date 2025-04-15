c1c11fc05a0bc17c1a6969531d04ae9a10aa2558
Molly Herforth <molly@Mollys-MacBook-Pro.local>
2025-04-15 19:14:57 -0400

    adding vulnerable package for demo

--- UnitTests/bad_code.py
+++ UnitTests/bad_code.py
@@ -4,7 +4,7 @@ def bad():
     subprocess.call("ls", shell=True)
 
 # hardcoded token/password in code
-token = ";alkjdsalfjieajlh"  # nosec: B105
+token = ";alkjdsalfjieajlh"
 
 # manually working with files in temp directory is not safe
 temp_dir = "/tmp"
