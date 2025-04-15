60db22310780cb8321f6c48faf9bebd836ef57d1
Molly Herforth <molly@Mollys-MacBook-Pro.local>
2025-04-15 19:47:17 -0400

    testing for demo purposes

--- UnitTests/bad_code.py
+++ UnitTests/bad_code.py
@@ -4,7 +4,7 @@ def bad():
     subprocess.call("ls", shell=True)
 
 # hardcoded token/password in code
-token = ";alkjdsalfjieajlh"
+token = ";alkjdsalfjieajlh" #nosec b105
 
 # manually working with files in temp directory is not safe
 temp_dir = "/tmp"
