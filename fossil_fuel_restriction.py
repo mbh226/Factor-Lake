3d28f439eeda44854c497237075aa0013b38fe69
Zoe Barrington <zab22@cornell.edu>
2025-05-01 10:26:56 -0400

    update to add sector restriction files

---
+++ fossil_fuel_restriction.py
@@ -0,0 +1,8 @@
+def get_fossil_fuel_restriction():
+    response = input("\nRestrict fossil fuel companies? (Yes/No): ").strip().lower()
+    if response in ["yes", "y"]:
+        print("Fossil fuel restriction enabled.")
+        return True
+    else:
+        print("No fossil fuel restriction applied.")
+        return False
